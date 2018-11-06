5c334e8267bef6fa80fdc5b237f49c37fd30750d
zlucifer <dekxter000@gmail.com>
2018-11-07 00:46:31 +0700

    Update cctv.sh

--- cctv.sh
+++ cctv.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo Selamat datang kak, Siapa nick kaka? #tulisan keluar
+
+read nick #membaca nick yg ditulis
+
+
 echo "   ..  ...  ...  ...  ..      .          ..  ....."
 sleep 0.03
 echo "  ...  .... .... ....     %/\      .. ..  .....  ."
@@ -40,4 +45,37 @@ sleep 2
 echo " ==          Eye Of All Seeing Project         =="
 sleep 0.7
 echo " ================================================"
+echo Selamat datang $nick ":)" Jones! :v
+echo
+echo "Kaka jones kan? :p"
+echo "jawab?"
+read jawab
+echo
+echo "NGAKAK DASAR JONES !!! :p"
+echo
+echo "Mulai scanning cctv?"
+echo "y/n"
+read confirm
+if [ $confirm = "y" ]; then
+echo "Melakukan scanning"
+i=0
+max=100
+while [ $i -le $max ]; do
+echo -ne "\nScanning percentage : $i% "
+sleep 0.03
+if [ $i -eq 100 ]; then
+echo -ne " [complete!]\n"
+echo "========================================"
+curl -s http://zlucifer.hol.es/Project_eye_of_all_seeing/index.php
+echo " Gunakan tools dengan bijak"
+echo
+echo " Love u always "
+echo " -zLucifer"
+echo "======================================="
+fi
+let i++
+done
+else
+echo "Kesalahan"
+fi
 
